70666c5a39288d8d268c255a099ffac1bff32ad2
LiveLifeA3D <christiansgueglia02@gmail.com>
2026-02-11 00:20:07 +0100

    test
    test

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const qtyTotalEl = $("qtyTotal");
 const buyTotalEl = $("buyTotal");
 const profitTotalEl = $("profitTotal");
 
-// ✅ nuovi totali
+// ✅ nuovi totali test
 const profitRealizedEl = $("profitRealized");
 const profitAllEl = $("profitAll");
 
